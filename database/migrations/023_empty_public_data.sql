40bed7fbc2ff7f96f532216ef6e2b3dcdf40e261
maahiraanka <56369611+maahiraanka@users.noreply.github.com>
2026-09-25 09:38:40 +1000

    feat: empty public competition content and supporter activity

---
+++ database/migrations/023_empty_public_data.sql
@@ -0,0 +1,33 @@
+DELETE FROM competition_stage_events;
+DELETE FROM competition_stage_choices;
+DELETE FROM competition_stages;
+DELETE FROM competition_referrals;
+DELETE FROM competition_device_claims;
+DELETE FROM competition_supporters;
+DELETE FROM competition_nominations;
+DELETE FROM competition_choices;
+DELETE FROM competitions;
+
+DELETE FROM competition_awards;
+DELETE FROM awards;
+DELETE FROM scoring_events;
+DELETE FROM match_assists;
+DELETE FROM match_state_events;
+DELETE FROM match_participations;
+DELETE FROM tournament_advancement_slots;
+DELETE FROM matches;
+DELETE FROM tournament_group_cities;
+DELETE FROM tournament_groups;
+DELETE FROM tournament_events;
+DELETE FROM tournament_stages;
+
+DELETE FROM qualification_referrals;
+DELETE FROM qualification_events;
+DELETE FROM city_memberships;
+DELETE FROM season_device_claims;
+DELETE FROM identity_integrity_events;
+DELETE FROM funnel_events;
+DELETE FROM moderation_cases;
+
+UPDATE users SET home_city_id=NULL WHERE home_city_id IS NOT NULL;
+DELETE FROM cities;
