102f1ab4497bee02d0b31415aff3f3101467f720
maahiraanka <56369611+maahiraanka@users.noreply.github.com>
2026-09-25 05:28:10 +1000

    feat: route /admin to Competition Control Centre

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import AdminApp from './AdminApp';
 import './styles.css';
-createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
+const Root=window.location.pathname.startsWith('/admin')?AdminApp:App;
+createRoot(document.getElementById('root')).render(<React.StrictMode><Root/></React.StrictMode>);
